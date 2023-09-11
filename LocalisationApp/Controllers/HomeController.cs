be8e6c2b93f8dc1349709dae00794f9f2a43d0c9
Zoran <38720227+zoka1997@users.noreply.github.com>
2023-09-11 12:09:36 +0200

    language data

---
+++ LocalisationApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using LocalisationApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System.Diagnostics;
+
+namespace LocalisationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<HomeController> _localiser;
+
+        public HomeController(ILogger<HomeController> logger, 
+            IStringLocalizer<HomeController> localizer)
+        {
+            _logger = logger;
+            _localiser = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            var localizedTitle = _localiser["Welcome"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LocalisationApp/Program.cs
@@ -0,0 +1,48 @@
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix);
+//Suffix sarch index language file
+builder.Services.AddLocalization(options =>
+{
+    options.ResourcesPath = "Resources";
+});
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[]
+    {
+        new CultureInfo("en-US"),
+        new CultureInfo("de-DE")
+    };
+    options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("en-US");
+    //This is our default language
+    options.SupportedCultures = supportedCultures;
+});
+
+var app = builder.Build();
+app.UseRequestLocalization();
+ 
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
